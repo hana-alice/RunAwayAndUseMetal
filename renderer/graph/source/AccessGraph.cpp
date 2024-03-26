c76db782bde805e3dd65ec43cb732c46052d1040
Zeqiang Li <zeqiang-li@outlook.com.com>
2024-03-26 22:52:46 +0800

    update

--- renderer/graph/source/AccessGraph.cpp
+++ renderer/graph/source/AccessGraph.cpp
@@ -1,6 +1,8 @@
 #include "AccessGraph.h"
 #include <boost/graph/depth_first_search.hpp>
 #include "RHIBuffer.h"
+#include "RHIImageView.h"
+#include "RHIImage.h"
 
 namespace raum::graph {
 
@@ -68,10 +70,65 @@ rhi::ImageLayout getImageLayout(rhi::AccessFlags flags) {
     return imgLayout;
 }
 
+rhi::Format getFormat(const Resource& res) {
+    rhi::Format format{rhi::Format::UNKNOWN};
+    std::visit(
+        overloaded{
+            [&format](const ImageData& img) {
+                format = img.info.format;
+            },
+            [&format](const ImageViewData& imgView) {
+                format = imgView.info.format;
+            },
+            [](auto) {
+            },
+        }, res.data);
+    return format;
+}
+
+uint32_t getSampleCount(const Resource& res) {
+    uint32_t samples{1};
+    std::visit(
+        overloaded{
+            [&samples](const ImageData& img) {
+                samples = img.info.sampleCount;
+            },
+            [&samples](const ImageViewData& imgView) {
+                samples = imgView.imageView->image()->info().sampleCount;
+            },
+            [](auto) {
+            },
+        },
+        res.data);
+    return samples;
+}
+
+auto decomposeDetail(const Resource& res) {
+    uint32_t samples{1};
+    rhi::Format format{rhi::Format::UNKNOWN};
+    std::visit(
+        overloaded{
+            [&samples, &format](const ImageData& img) {
+                samples = img.info.sampleCount;
+                format = img.info.format;
+            },
+            [&samples, &format](const ImageViewData& imgView) {
+                samples = imgView.imageView->image()->info().sampleCount;
+                format = imgView.info.format;
+            },
+            [](auto) {
+            },
+        },
+        res.data);
+    return std::tie(samples, format);
+}
+
 struct AccessVisitor : public boost::dfs_visitor<> {
 public:
     void discover_vertex(const RenderGraphImpl::vertex_descriptor v, const RenderGraphImpl& rg) {
         if (std::holds_alternative<RenderPassData>(rg[v].data)) {
+            auto [fbIter, _] = _fbInfoMap.emplace(std::piecewise_construct, std::forward_as_tuple(v), std::forward_as_tuple());
+
             const auto& data = std::get<RenderPassData>(rg[v].data);
             for (const auto& res : data.attachments) {
                 // convention: "" represents color/depth outputs
@@ -107,12 +164,23 @@ public:
                         stage |= rhi::PipelineStage::COLOR_ATTACHMENT_OUTPUT;
                     }
                 }
+                const auto& resourceDetail = _resg.get(res.name);
+                auto [sampleCount, format] = decomposeDetail(resourceDetail);
+
+                auto [iter, _] = _rpInfoMap.emplace(std::piecewise_construct, std::forward_as_tuple(v), std::forward_as_tuple());
+                iter->second.attachments.emplace_back(
+                    res.loadOp,
+                    res.storeOp,
+                    res.stencilLoadOp,
+                    res.stencilStoreOp,
+                    format,
+                    sampleCount,
+                    getImageLayout(access),
+                    getImageLayout(access)
+                );
+
+                //fbIter->
                 _accessMap[res.name].emplace_back(v, access, stage);
-
-                rhi::RenderPassInfo rpInfo{};
-
-
-
             }
         } else if (std::holds_alternative<RenderQueueData>(rg[v].data)) {
             const auto& data = std::get<RenderQueueData>(rg[v].data);
@@ -154,6 +222,8 @@ public:
     const ResourceGraph& _resg;
     const ShaderGraph& _sg;
     AccessGraph::ResourceAccessMap& _accessMap;
+    AccessGraph::RenderPassInfoMap& _rpInfoMap;
+    AccessGraph::FrameBufferInfoMap& _fbInfoMap;
 };
 
 void populateBarrier(const AccessGraph::ResourceAccessMap& accessMap,
@@ -253,7 +323,7 @@ void AccessGraph::analyze() {
     auto indexMap = boost::get(boost::vertex_index, _rg.impl());
     auto colorMap = boost::make_vector_property_map<boost::default_color_type>(indexMap);
 
-    AccessVisitor visitor{{}, _resg, _sg, _accessMap};
+    AccessVisitor visitor{{}, _resg, _sg, _accessMap, _renderPassInfoMap, _frameBufferInfoMap};
     for (auto vertex : boost::make_iterator_range(boost::vertices(_rg.impl()))) {
         if (std::holds_alternative<RenderPassData>(_rg.impl()[vertex].data)) {
             boost::depth_first_visit(_rg.impl(), 0, visitor, colorMap);
